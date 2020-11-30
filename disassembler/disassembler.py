444550cd9ffc5e1f913669534c342f27267e1a48
Asudy Wang <18906052155@189.cn>
2020-11-30 09:33:23 +0800

    Add an option for generating 'R*' style regs

--- disassembler/disassembler.py
+++ disassembler/disassembler.py
@@ -44,6 +44,8 @@ ITypeCode = [k for k in OpCode.keys() if k not in JTypeCode]
 
 Regs = ["zero", "at", "v0", "v1", "a0", "a1", "a2", "a3", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", \
         "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"]
+# Regs = ["R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15", \
+#         "R16", "R17", "R18", "R19", "R20", "R21", "R22", "R23", "R24", "R25", "R26", "R27", "R28", "R29", "R30", "R31"]
 
 for i in range( len(lines) ):   # Suppose each line is a base 16 instruction
     # inst = input()      # Input base 16 instruction
